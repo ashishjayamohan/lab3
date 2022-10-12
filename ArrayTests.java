c333181413987066ebe16f0d6d42b6ef681539cf
Ashish Jayamohan <ashishjayamohan@Ashishs-MacBook-Pro.local>
2022-10-12 14:35:54 -0700

    array tests

--- ArrayTests.java
+++ ArrayTests.java
@@ -15,4 +15,17 @@ public class ArrayTests {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+
+  @Test
+  public void testReverse() {
+    int[] a = {3, 1, 5};
+    assertArrayEquals(new int[]{5, 1, 3}, ArrayExamples.reversed(a));
+  }
+
+  @Test
+  public void testReverseInPlace2() {
+    int[] a = {3, 1, 5};
+    ArrayExamples.reverseInPlace(a);
+    assertArrayEquals(new int[]{5, 1, 3}, a);
+  }
 }
